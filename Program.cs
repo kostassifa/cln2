32065eb4605c12912e8553c6447461ae355d42c1
konstantinos sifa <sifavs@yahoo.com>
2020-11-01 14:43:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Threading;
+
+namespace ESY_XEREIS_TO_TAIRI_SOU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal count = 0;
+            decimal temp = 0;
+            decimal temp1 = 0;
+            decimal temp2= 0;
+            decimal temp3= 0;
+            decimal temp4= 0;
+            int milliseconds = 5000;
+            decimal pososto = 0;
+            decimal epitisekato = 0;
+            Console.WriteLine("KALWS HRTHES STO GNWRISE TO TAIRI SOU!!!!TO PAIXNIDI POU EITE XVRIZEI ZEYGARIA EITE TA PANTREVEI!!!KALH SOY TYXH THA THN XREIASTEIS!!!");
+            Thread.Sleep(milliseconds);
+            Console.Write("(simantiki pliroforia:to paixnidi den analamvanei tin euthini ama eseis xwrisete)");
+            Thread.Sleep(milliseconds);
+            Console.Write("\n"+"TWRA POY XEREIS TOUS OROUS EISAI SIGOYROS OTI THES NA PAIXEIS??");
+            //Thread.Sleep(milliseconds);
+            Console.Write("\n"+"GRAPSE NAI H OXI: ");
+            string x = Console.ReadLine();
+            string apantisi = x.ToUpper();
+            if (apantisi == "NAI")
+            {
+                int i = 0;
+                while (true)
+                {
+                    Console.Write("\n" + "ERWTHSH 1:POTE EPIASE DOULEIA PTWRH FORA?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string y = Console.ReadLine();
+                    if (y == "1973")
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                        temp = count;
+                        //Console.Write("\n"+count);
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                       // Console.Write("\n" + count);
+                    }
+                    Console.Write("\n" + "ERWTHSH 2:POY EIXAME PAEI PERISSOTERES FORES PRIN TO GAMO kalokairi?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string p = Console.ReadLine();
+                    if ((p == "spetses") || (p == "SPETSES"))
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                        //temp = count;
+                       // Console.Write("\n" + count);
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                       // Console.Write("\n" + count);
+                    }
+                    Console.Write("\n" + "ERWTHSH 3:POTE parousiastika fantaros ?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string l = Console.ReadLine();
+                    if ((l == "30 11 1981") || (l == "31/11/1981") || (l == "30-11-1981"))
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                        temp2 = temp1 + count;
+                       // Console.Write("\n" + count);
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                        //Console.Write("\n" + count);
+                    }
+                    Console.Write("\n" + "ERWTHSH 4:POTE EXeis GENETHLIA?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string k = Console.ReadLine();
+                    if ((k == "15/7") || (k == "15-7"))
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                        temp3 = temp2 + count;
+                       // Console.Write("\n" + count);
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                       // Console.Write("\n" + count);
+                    }
+                    Console.Write("\n" + "ERWTHSH 5:POTE phres to prwto sou amaxi?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string n = Console.ReadLine();
+                    if (n =="1982")
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                        temp4 = temp3 + count;
+                        //Console.Write("\n" + count);
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                        //Console.Write("\n" + count);
+                    }
+                    Console.Write("\n" + "ERWTHSH 6:poio einai to agapimeno eidos theatrou?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string b = Console.ReadLine();
+                    if (b == "kwmwdies")
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                    }
+                    Console.Write("\n" + "ERWTHSH 7:TI NOYMERO PAPOUTSI FORAEI?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string m = Console.ReadLine();
+                    if (m == "43")
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                    }
+                    Console.Write("\n" + "ERWTHSH 8:POIO EINAI TO HOBBIE THS?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string z = Console.ReadLine();
+                    if (z == "podosfairo")
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                    }
+                    Console.Write("\n" + "ERWTHSH 9:otan douleva pote agorasa to podhlato ths douleias?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string glyko = Console.ReadLine();
+                    if (glyko == "1976")
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                    }
+                    Console.Write("\n" + "ERWTHSH 10:POIO EINAI TO AGAPHMENO THS XRWMA?");
+                    Console.Write("\n" + "GRAPSE THN APANTHSH SOU: ");
+                    string xrwma = Console.ReadLine();
+                    if (xrwma == "mple")
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "SYNXARHTHRIA APANTISES SWSTA");
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        Thread.Sleep(milliseconds);
+                        Console.Write("\n" + "TI SOK??APANTISES LATHOS");
+                        temp = count;
+                    }
+
+
+                    //Console.Write("\n" + count);
+                    pososto = count / 10;
+                    //Console.Write("\n" + pososto);
+                    epitisekato = pososto * 100;
+                    //Console.Write("\n" + epitisekato);
+                    Console.Write("\n" + "TO POSOSTO SOU EPITYXIAS EINAI" + " " + epitisekato + "%");
+
+                    if (epitisekato < 20)
+                    {
+                        Console.Write("\n" + "SE PROEIDOPOIHSAME POLLES FORES NA MHN PAIXEIS DEN AKOUSES");
+                    }
+                    else if ((epitisekato > 20) && (epitisekato <= 50))
+                    {
+                        Console.Write("\n" + "E KAMIA EVDOMADA THA KRATHSEI H MOURMOURA YPOMONH");
+                    }
+                    else if ((epitisekato > 50) && (epitisekato <= 80))
+                    {
+                        Console.Write("\n" + "AAAA DEN EINAI KAKO,DEN EINAI KAKO,KATI EKANES");
+                    }
+                    else if ((epitisekato > 80) && (epitisekato < 100))
+                    {
+                        Console.Write("\n" + "TI LES RE SKYLE ESY EISAI KELEPOURI TA VLEPEIS ESY POU TON KRAZEIS,OXI DES TA TWRA");
+                    }
+                    else if (epitisekato == 100)
+                    {
+                        Console.Write("\n" + "PAIDIA VRIKAME ENAN GIA TO MOUSEIO TREXTE,PANTREPSOU AN DEN TO EXEIS KANEI HDH,KAI AYTO POU SYZHTATE KAIRO NOMIZW HRTHE H WRA NA TOU TO KANEIS (: (:");
+                    }
+                    i++;
+                    if (i == 1)
+                        break;
+                }
+            }
+            else
+            {
+                Console.Write("\n" + "MRAVO EKANES THN KALYTERH EPILOGH MALLON THA GAMHSEIS KAI SHMERA (: ");
+            }
+           
+        }
+    }
+}
